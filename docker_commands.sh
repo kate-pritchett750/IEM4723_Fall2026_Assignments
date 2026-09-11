0d5e023916adef31d9215763278c3038dfe51e85
kate-pritchett750 <kate.pritchett@okstate.edu>
2026-09-11 12:44:19 -0500

    Complete Question 4 port forwarding

--- docker_commands.sh
+++ docker_commands.sh
@@ -12,3 +12,7 @@ docker run --rm --volume "%cd%\data:/home/iem4723/data" volume-mounting-app
 # Question 3
 docker build -t logistic_regression_layered:latest .
 docker run --rm --volume "%cd%\data:/home/iem4723/data" logistic_regression_layered:latest
+
+# Question 4
+docker build -t simple-file-server .
+docker run --rm --publish 3333:8000 simple-file-server
